b250397316968b53462c26788fbade242c2e596a
dudka3654 <dudka3654@gmail.com>
2025-07-21 06:03:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('content-container');
     const userLang = navigator.language || navigator.userLanguage;
 
-    // Баннер с промокодом (текст поверх изображения)
     let heroText = '<div style="position: relative;">';
     heroText += '<img src="banner.png" alt="Баннер" class="banner">';
     heroText += '<div class="promo-code" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">Промокод: SPINS400</div>';
     heroText += '</div>';
     heroText += '<h1>Забери 400 БЕСПЛАТНЫХ ВРАЩЕНИЙ!</h1>';
     heroText += '<p>Важно: Введи промокод перед входом!</p>';
-    heroText += '<button onclick="openModal()" class="button-img" style="background-image: url(\'bonus-box-btn.png\');"></button>';
+    heroText += '<button onclick="openModal()" class="button-img" style="background-image: url(\'bonus-box-btn.png\');" data-promo="BOX400"></button>';
 
-    // 4 кнопки с фото
     let bonuses = '';
     if (userLang.startsWith('ru') || userLang.startsWith('kz')) {
-        bonuses += '<a href="https://example.com/irwin"><img src="irwin-btn.png" alt="Irwin" class="button-img"></a>';
-        bonuses += '<a href="https://example.com/flagman"><img src="flagman-btn.png" alt="Flagman" class="button-img"></a>';
-        bonuses += '<a href="https://example.com/martin"><img src="martin-btn.png" alt="Martin" class="button-img"></a>';
-        bonuses += '<a href="https://example.com/bonus"><img src="bonus-btn.png" alt="Bonus" class="button-img"></a>';  // 4-я кнопка
+        bonuses += '<a href="https://example.com/irwin"><img src="irwin-btn.png" alt="Irwin" class="button-img" data-promo="IRWIN400"></a>';
+        bonuses += '<a href="https://example.com/flagman"><img src="flagman-btn.png" alt="Flagman" class="button-img" data-promo="FLAG400"></a>';
+        bonuses += '<a href="https://example.com/martin"><img src="martin-btn.png" alt="Martin" class="button-img" data-promo="MARTIN400"></a>';
+        bonuses += '<a href="https://example.com/bonus"><img src="bonus-btn.png" alt="Bonus" class="button-img" data-promo="BONUS400"></a>';
     } else {
-        bonuses += '<a href="https://example.com/global"><img src="bonus-btn.png" alt="Global" class="button-img"></a>';
+        bonuses += '<a href="https://example.com/global"><img src="bonus-btn.png" alt="Global" class="button-img" data-promo="GLOBAL400"></a>';
     }
 
     let testimonials = '<div class="testimonial">"Выиграл 5000!" - Алексей</div>';
     testimonials += '<div class="testimonial">"Лучшие бонусы!" - Anna</div>';
 
-    let footer = '<a href="https://t.me/yourchannel"><img src="telegram-btn.png" alt="Telegram" class="button-img"></a>';
+    let footer = '<a href="https://t.me/yourchannel"><img src="telegram-btn.png" alt="Telegram" class="button-img" data-promo="TGJOIN"></a>';
 
     const dynamicContent = heroText + bonuses + testimonials + footer;
 
